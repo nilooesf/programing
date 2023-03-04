25513f26a353e8a721c7a34ec9212293dd308a07
nilooesf <86556214+nilooesf@users.noreply.github.com>
2023-03-04 21:30:58 +0330

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,16 @@
+using system;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            int num1=Convert.ToInt32(Console.ReadLine());
+            int num2=Convert.ToInt32(Console.ReadLine());
+            int sum =num1+num2;
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
