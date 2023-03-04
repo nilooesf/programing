31350da78ef8e39d4f20868a9d7752d32adf2551
nilooesf <86556214+nilooesf@users.noreply.github.com>
2023-03-04 20:02:50 +0330

    Add files via upload

---
+++ determinan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[3][3],i,j;
+    int det=0;
+    cout<<"enter matrix"<<endl;
+    for(i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    det=a[0][0]*((a[1][1]*a[2][2])
+    -(a[2][1])*a[1][2])
+    -a[0][1]*((a[1][0]*a[2][2])
+    -(a[2][0])*a[1][2])+a[0][2]*((a[1][0])
+    *a[2][1]-(a[2][0])*a[1][1]);
+    cout<<"the determinan is :"<<det<<endl;
+return 0;
+
+}
